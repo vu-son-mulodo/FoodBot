ac947429863a940510c3a8259a47272894df6fa6
Prateek Srivastava <f2prateek@gmail.com>
2013-02-10 09:59:15 -0700

    Add util classes

---
+++ app/src/main/java/com/f2prateek/foodbot/util/AppConstants.java
@@ -0,0 +1,8 @@
+package com.f2prateek.foodbot.util;
+
+public class AppConstants {
+
+	public static final String STAT_TOTAL_DAYS = "com.f2prateek.foodbot.stats.days";
+	public static final String STAT_TOTAL_CALORIES = "com.f2prateek.foodbot.stats.total_calories";
+
+}
